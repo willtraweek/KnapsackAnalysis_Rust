7403573799bc528a6b9ca3ba71b5544a63cae1b1
Will Traweek <will.traweek@gmail.com>
2021-02-13 23:25:21 -0600

    add package creation and getters

--- src/Package.rs
+++ src/Package.rs
@@ -5,5 +5,23 @@ pub struct Package {
 }
 
 impl Package {
+    pub fn new(name: String, weight: u16, price: u16) -> Package {
+        Package {
+            name,
+            weight,
+            price
+        }
+    }
 
+    pub fn name(&self) -> &String {
+        &self.name
+    }
+
+    pub fn weight(&self) -> weight {
+        &self.weight
+    }
+
+    pub fn price(&self) -> weight {
+        &self.price
+    }
 }
