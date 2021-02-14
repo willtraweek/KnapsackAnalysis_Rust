3e23b393458f7783fe57a9be675d0c93b278eba5
Will Traweek <will.traweek@gmail.com>
2021-02-14 01:27:11 -0600

    add Truck comparison

--- src/truck.rs
+++ src/truck.rs
@@ -1,6 +1,8 @@
 use crate::package::Package as Package;
 use std::collections::HashMap;
+use std::cmp::{Ordering, Eq, PartialOrd, PartialEq};
 
+#[derive(Eq)]
 pub struct Truck {
     //! Truck to be filled with packages
     //!
@@ -15,3 +17,29 @@ pub struct Truck {
     boxes: HashMap<String, u16>
 }
 
+
+impl Ord for Truck {
+    fn cmp (&self, other: &Self) -> Ordering {
+        if self.capacity < other.capacity {
+            return Ordering::Less;
+        } else if self.weight < other.weight {
+            return Ordering::Less;
+        } else if self.value < other.value {
+            return Ordering::Less;
+        }
+
+        Ordering::Greater
+    }
+}
+
+impl PartialOrd for Truck {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Truck {
+    fn eq(&self, other: &Self) -> bool {
+        return (self.capacity == other.capacity) && (self.weight == other.weight) && (self.value == other.value)
+    }
+}
