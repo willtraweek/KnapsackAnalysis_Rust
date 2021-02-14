9fca55ea1110dfd2773dbce78bc0a9d52d7fb7ea
Will Traweek <will.traweek@gmail.com>
2021-02-14 02:35:52 -0600

    add semicolon to main

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ mod package;
 mod truck;
 
 fn main() {
-    utilities::set_creation::create_test_set(10, 100, 500, Some(10))
+    utilities::set_creation::create_test_set(10, 100, 500, Some(10));
 }
