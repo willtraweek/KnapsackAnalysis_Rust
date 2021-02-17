b6c853bf8e235af70f8c76e1e53f90c38695ca4c
Will Traweek <will.traweek@gmail.com>
2021-02-17 07:32:34 -0600

    add knapsack::greedy_solution()

--- src/knapsack.rs
+++ src/knapsack.rs
@@ -1,13 +1,12 @@
 use crate::truck::Truck;
 use crate::warehouse::Warehouse;
-use crate::package::Package;
 
-fn greedy_solution(warehouse: &mut Warehouse, truck: &mut Truck) {
+pub fn greedy_solution(warehouse: &mut Warehouse, truck: &mut Truck) {
     // ALWAYS LOADS THE MOST EXPENSIVE BOX INTO THE TRUCK
     while (warehouse.has_packages()) && (truck.leftover_space() > 0) {
-        match warehouse.load_most_expensive_box(truck) {
-            true => 0,  // DO NOTHING, THE OPERATION WAS A SUCCESS
-            false => return // THE OPERATION IS COMPLETE, SINCE THERE ARE NO MORE BOXES TO LOAD
+        let boxes_left =  warehouse.load_most_expensive_box(truck);
+        if !boxes_left {
+            return;  // THE OPERATION IS COMPLETE, SINCE THERE ARE NO MORE BOXES TO LOAD
         }
     }
 }
